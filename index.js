d67c6a23ae135a465e93deaff3e605fc22d6576a
Gillan <gillanropero@gmail.com>
2026-01-14 12:35:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
     $('.share').on('click', function() {
         // Toggle the visibility of the share options
-        $('.share-options').toggle();
+        $('.share-options').fadeIn();
     });
 
     // Optional: Close the share options if the user clicks anywhere else on the page
     $(document).on('click', function(event) {
-        if (!$(event.target).closest('.button').length) {
-            $('.share-options').hide();
+        if (!$(event.target).closest('.share').length) {
+            $('.share-options').fadeOut();
         }
     });
